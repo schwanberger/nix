447726fada5622d96d3de7b746e0e2b2e84066cb
Thomas Schwanberger <thsc@netcompany.com>
2023-09-30 20:52:04 +0000

    Iteration

--- flake.nix
+++ flake.nix
@@ -12,28 +12,25 @@
       system = "x86_64-linux";
       pkgs = import inputs.nixpkgs { inherit system; };
     in
-    {
-      nixosConfigurations."PF3LZDKP" = inputs.nixpkgs.lib.nixosSystem {
-        inherit system;
-        modules = [
-          ./configuration.nix
-          inputs.nixos-wsl.nixosModules.wsl
-          {
-            system.stateVersion = "23.05";
-            wsl = {
-              enable = true;
-              defaultUser = "thsc";
-              nativeSystemd = true;
-              wslConf = {
-                network.hostname = "PF3LZDKP";
-                interop.enabled = true;
+      {
+        nixosConfigurations."PF3LZDKP" = inputs.nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+            inputs.nixos-wsl.nixosModules.wsl
+            {
+              system.stateVersion = "23.05";
+              wsl = {
+                enable = true;
+                defaultUser = "thsc";
+                nativeSystemd = true;
+                wslConf = {
+                  network.hostname = "PF3LZDKP";
+                  interop.enabled = true;
+                };
               };
-            };
-            users.users."thsc" = {
-              isNormalUser = true;
-            };
-          }
-        ];
+            }
+          ];
+        };
       };
-    };
 }
