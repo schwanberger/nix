b06adc918eaa8d23e5dbbada68f0ee17930c0d35
Thomas Schwanberger <thsc@netcompany.com>
2023-10-01 00:00:48 +0000

    Prepare for multi hosts, use home manager

--- flake.nix
+++ flake.nix
@@ -5,32 +5,30 @@
     nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
     nixos-wsl.url = "github:nix-community/NixOS-WSL";
     nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager.url = "github:nix-community/home-manager/release-23.05";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, ... }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = import inputs.nixpkgs { inherit system; };
-    in
-      {
-        nixosConfigurations."PF3LZDKP" = inputs.nixpkgs.lib.nixosSystem {
-          inherit system;
-          modules = [
-            ./configuration.nix
-            inputs.nixos-wsl.nixosModules.wsl
-            {
-              system.stateVersion = "23.05";
-              wsl = {
-                enable = true;
-                defaultUser = "thsc";
-                nativeSystemd = true;
-                wslConf = {
-                  network.hostname = "PF3LZDKP";
-                  interop.enabled = true;
-                };
+  outputs = { self, nixpkgs, ... }@inputs:
+    {
+      nixosConfigurations."PF3LZDKP" = nixpkgs.lib.nixosSystem {
+        specialArgs  = { inherit inputs; };
+        system = "x86_64-linux";
+        modules = [
+          ./nixos-wsl/configuration.nix
+          inputs.nixos-wsl.nixosModules.wsl
+          {
+            wsl = {
+              enable = true;
+              defaultUser = "thsc";
+              nativeSystemd = true;
+              wslConf = {
+                network.hostname = "PF3LZDKP";
+                interop.enabled = true;
               };
-            }
-          ];
-        };
+            };
+          }
+        ];
       };
+    };
 }
---
+++ nixos-wsl/configuration.nix
@@ -0,0 +1,45 @@
+{ inputs, config, pkgs, ... }:
+
+{
+  imports = [
+    inputs.home-manager.nixosModules.home-manager
+  ];
+  security.pam.loginLimits = [{
+    domain = "*";
+    type = "soft";
+    item = "nofile";
+    value = "8192";
+  }];
+  environment.systemPackages = [ pkgs.vim pkgs.home-manager pkgs.zsh ];
+  environment.shells = with pkgs; [ zsh ];
+  programs.git.enable = true;
+
+  programs = {
+    zsh = {
+      enable = true;
+      ohMyZsh = {
+        enable = true;
+        theme = "robbyrussell";
+        plugins = [
+          "git"
+          "fzf"
+        ];
+      };
+    };
+  };
+
+  users.users.thsc = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+  };
+
+  nix.settings.experimental-features = [ "nix-command" "flakes" ];
+  system.stateVersion = "23.05";
+
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      thsc = import ./home.nix;
+    };
+  };
+}
