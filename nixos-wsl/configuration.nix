605a4e04b1b84462095d030dbad0405798e07967
Thomas Schwanberger <thsc@netcompany.com>
2023-11-15 14:34:10 +0100

    Add some podman

--- nixos-wsl/configuration.nix
+++ nixos-wsl/configuration.nix
@@ -36,6 +36,15 @@
   };
 
   virtualisation.containers.enable = true;
+  virtualisation.podman = {
+      enable = true;
+
+      # Create a `docker` alias for podman, to use it as a drop-in replacement
+      dockerCompat = true;
+
+      # Required for containers under podman-compose to be able to talk to each other.
+      defaultNetwork.settings.dns_enabled = true;
+  };
   # Will have to wait until Windows 11 WSL2
   #virtualisation.libvirtd.enable = true;
   #virtualisation.virtualbox.host.enable = true;
