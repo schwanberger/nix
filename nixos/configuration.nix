4f19d1e348f612cbd4abb26856783a4a41a75830
Thomas Schwanberger <thsc@netcompany.com>
2023-12-06 01:48:41 +0100

    Remove initialPassword and comment

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -140,10 +140,6 @@
   users.defaultUserShell = pkgs.zsh;
   users.users = {
     thsc = {
-      # TODO: You can set an initial password for your user.
-      # If you do, you can skip setting a root password by passing '--no-root-passwd' to nixos-install.
-      # Be sure to change it (using passwd) after rebooting!
-      initialPassword = "correcthorsebatterystaple";
       isNormalUser = true;
       openssh.authorizedKeys.keys = [
       ];
