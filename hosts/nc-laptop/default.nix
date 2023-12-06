0ac1cc3974b3d6aaa2e07b4aa7805487cddfa097
Thomas Schwanberger <thsc@netcompany.com>
2023-12-06 09:12:09 +0100

    Enable substituters in module as well

--- hosts/nc-laptop/default.nix
+++ hosts/nc-laptop/default.nix
@@ -55,12 +55,20 @@
     value.source = value.flake;
   }) config.nix.registry;
 
-  nix.settings = {
-    # Enable flakes and new 'nix' command
-    experimental-features = "nix-command flakes";
-    # Deduplicate and optimize nix store
-    auto-optimise-store = true;
-    trusted-users = [ "thsc" ];
+  nix = {
+    settings = {
+      # Enable flakes and new 'nix' command
+      experimental-features = "nix-command flakes";
+      # Deduplicate and optimize nix store
+      auto-optimise-store = true;
+      substituters =
+        [ "https://nix-community.cachix.org" "https://devenv.cachix.org" ];
+      trusted-public-keys = [
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
+      ];
+      trusted-users = [ "root" "@wheel" ];
+    };
   };
 
   nix.optimise.automatic = true;
