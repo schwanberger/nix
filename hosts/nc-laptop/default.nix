0ef1ad744fe416485418fbc2716d83702b14e0e2
Thomas Schwanberger <thomas@schwanberger.dk>
2024-07-01 21:56:51 +0200

    Clean up opengl

--- hosts/nc-laptop/default.nix
+++ hosts/nc-laptop/default.nix
@@ -255,18 +255,7 @@
 
   services.xserver.videoDrivers = lib.mkDefault [ "modesetting" ];
 
-  # hardware.opengl = {
-  #   enable = true;
-  #   extraPackages = with pkgs; [ intel-media-driver intel-ocl ];
-  # };
-
-  # From: https://github.com/Atry/nixos-wsl-vscode/blob/main/flake.nix
-  hardware.opengl = {
-    enable = true;
-    driSupport = true;
-    driSupport32Bit = true;
-    setLdLibraryPath = true;
-  };
+  hardware.opengl = { enable = true; };
 
   fonts = {
     enableDefaultPackages = true;
