4bfe9dbb42522ffbcc6beccd34f377122ee1c174
Thomas Schwanberger <thomas@schwanberger.dk>
2024-10-24 21:24:23 +0200

    Bump deps but lock sops-nix due to bug

--- flake.nix
+++ flake.nix
@@ -57,7 +57,7 @@
     #   inputs.nixpkgs.follows = "nixpkgs";
     # };
 
-    sops-nix.url = "github:Mic92/sops-nix";
+    sops-nix.url = "github:Mic92/sops-nix?ref=a4c33bfecb93458d90f9eb26f1cf695b47285243";
     sops-nix.inputs.nixpkgs.follows = "nixpkgs";
 
     # TODO: Add any other flake you might need
