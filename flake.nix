f3fcd9a0e69e49306479b25f5020f069dada2940
Thomas Schwanberger <thomas@schwanberger.dk>
2026-01-05 12:18:36 +0100

    feat: explicit substituter config

--- flake.nix
+++ flake.nix
@@ -5,12 +5,12 @@
     extra-substituters = [
       "https://nix-community.cachix.org"
       "https://devenv.cachix.org"
-      # "https://install.determinate.systems"
+      "https://install.determinate.systems"
     ];
     extra-trusted-public-keys = [
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw="
-      # "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
+      "cache.flakehub.com-3:hJuILl5sVK4iKm86JzgdXW12Y2Hwd5G07qKtHTOcDCM="
     ];
   };
 
